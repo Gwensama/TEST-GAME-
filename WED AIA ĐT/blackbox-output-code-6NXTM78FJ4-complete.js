ddbe6ceb85faa1090e204729563d611195bfe853
Gwensama <97827922+Gwensama@users.noreply.github.com>
2026-04-24 01:27:46 +0700

    Delete WED AIA ĐT directory

--- WED AIA ĐT/blackbox-output-code-6NXTM78FJ4-complete.js
+++
@@ -1,2 +0,0 @@
-// Hero Slider
-let currentSlide
--- WED AIA ĐT/blackbox-output-code-6NXTM78FJ4.js
+++
@@ -1,6 +0,0 @@
-// Hero Slider
-let currentSlide = 0;
-const slides = document.querySelectorAll('.slide');
-const indicators = document.querySelectorAll('.indicator');
-
-function showSlide(index)

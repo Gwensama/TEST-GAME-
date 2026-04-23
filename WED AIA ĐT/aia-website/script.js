5e22b8591f2c03ee1400fb932bc131433d33706a
Gwensama <97827922+Gwensama@users.noreply.github.com>
2026-04-24 01:27:22 +0700

    Delete WED AIA ĐT/aia-website directory

--- WED AIA ĐT/aia-website/script.js
+++
@@ -1,8 +0,0 @@
-// Hero Slider
-let currentSlide = 0;
-const slides = document.querySelectorAll('.slide');
-const indicators = document.querySelectorAll('.indicator');
-
-function showSlide(index) {
-  slides.forEach(slide => slide.classList.remove('active'));
-  indicators.forEach(ind => ind.classList
